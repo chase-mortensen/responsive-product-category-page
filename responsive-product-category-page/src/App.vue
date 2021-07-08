70fb54bad2f8e1960d0ae5494bcdfd20f6537807
chase-mortensen <chase@chase-mortensen.dev>
2021-07-08 12:50:08 -0600

    first hero section and product cards started

--- responsive-product-category-page/src/App.vue
+++ responsive-product-category-page/src/App.vue
@@ -1,10 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
+  <Hero />
+  <ProductCard />
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Hero from './components/Hero.vue'
+import ProductCard from './components/ProductCard.vue'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
